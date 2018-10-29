8630c2490d74260f0820c37c6aa9c64b6956a640
wojtekbu <bulawa.wojciech@gmail.com>
2018-10-29 01:45:56 +0100

    PD-67: Added sending Travel with a place

---
+++ app/src/main/java/podwozka/podwozka/Constants.java
@@ -0,0 +1,15 @@
+package podwozka.podwozka;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+
+public final class Constants {
+
+    public static ObjectMapper getDefaultObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setDateFormat(new StdDateFormat());
+        return mapper;
+    }
+}
