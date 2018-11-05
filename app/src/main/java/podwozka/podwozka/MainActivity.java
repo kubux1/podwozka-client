f3a365b3e82438fe3a9b26002cbbacbfa703f947
Jakub Kotowski <jakubkotow@gmail.com>
2018-11-05 22:06:31 +0100

    Update MainActivity.java

--- app/src/main/java/podwozka/podwozka/MainActivity.java
+++ app/src/main/java/podwozka/podwozka/MainActivity.java
@@ -143,10 +143,8 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // Enable the functionality that depends on this permission.
-                    int x = 2;
                 } else {
                     // Disable the functionality that depends on this permission.
-                    int y = 2;
                 }
                 return;
             }
